2fa5e35840e7be760912a7d1b3bb826f1d110979
roundedrectangle <roundedrectangle0@gmail.com>
2026-07-04 12:27:47 +0300

    update translation

--- translations/harbour-2048-ru.ts
+++ translations/harbour-2048-ru.ts
@@ -4,110 +4,90 @@
 <context>
     <name>ChangeModePage</name>
     <message>
-        <location filename="../qml/pages/ChangeModePage.qml" line="15"/>
         <source>Change Mode</source>
-        <translation type="unfinished">Сменить режим</translation>
+        <translation>Сменить режим</translation>
     </message>
     <message>
-        <location filename="../qml/pages/ChangeModePage.qml" line="19"/>
         <source>Difficulty</source>
-        <translation type="unfinished">Сложность</translation>
+        <translation>Сложность</translation>
     </message>
     <message>
-        <location filename="../qml/pages/ChangeModePage.qml" line="49"/>
         <source>Format</source>
-        <translation type="unfinished">Формат</translation>
+        <translation>Формат</translation>
     </message>
     <message>
-        <location filename="../qml/pages/ChangeModePage.qml" line="64"/>
         <source>Size</source>
-        <translation type="unfinished">Размер</translation>
+        <translation>Размер</translation>
     </message>
     <message>
-        <location filename="../qml/pages/ChangeModePage.qml" line="80"/>
         <source>Reset</source>
-        <translation type="unfinished">Сбросить</translation>
+        <translation>Сбросить</translation>
     </message>
     <message>
-        <location filename="../qml/pages/ChangeModePage.qml" line="83"/>
         <source>Data reset</source>
         <comment>past tense</comment>
-        <translation type="unfinished">Данные сброшены</translation>
+        <translation>Данные сброшены</translation>
     </message>
 </context>
 <context>
     <name>Lose</name>
     <message>
-        <location filename="../qml/components/Lose.qml" line="19"/>
         <source>No moves available</source>
-        <translation type="unfinished"></translation>
+        <translation>Ходов не осталось</translation>
     </message>
     <message>
-        <location filename="../qml/components/Lose.qml" line="25"/>
         <source>Best tile</source>
-        <translation type="unfinished"></translation>
+        <translation>Лучшая плитка</translation>
     </message>
     <message>
-        <location filename="../qml/components/Lose.qml" line="29"/>
         <source>Classic score</source>
-        <translation type="unfinished"></translation>
+        <translation>Классический счёт</translation>
     </message>
     <message>
-        <location filename="../qml/components/Lose.qml" line="33"/>
         <source>Improved score</source>
-        <translation type="unfinished"></translation>
+        <translation>Счёт улучшения</translation>
     </message>
     <message>
-        <location filename="../qml/components/Lose.qml" line="37"/>
         <source>Moves</source>
-        <translation type="unfinished"></translation>
+        <translation>Ходы</translation>
     </message>
 </context>
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="20"/>
         <source>Change Mode</source>
         <translation>Сменить режим</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="24"/>
         <source>Restart game</source>
         <translation>Перезапустить игру</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="34"/>
         <source>%1 difficulty, %2</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 сложность, %2</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="36"/>
         <source>%1x%1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1x%1</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="45"/>
         <source>SCORE</source>
         <translation>СЧЁТ</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="46"/>
         <source>TILE</source>
         <translation>ПЛИТКА</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="47"/>
         <source>MOVES</source>
         <translation>ХОДЫ</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="48"/>
         <source>IMPSCORE</source>
         <comment>short for improved score</comment>
         <translation>УЛУЧШСЧЁТ</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="87"/>
         <source>BEST</source>
         <translation>ЛУЧШЕЕ</translation>
     </message>
@@ -115,37 +95,30 @@
 <context>
     <name>harbour-2048</name>
     <message>
-        <location filename="../qml/harbour-2048.qml" line="12"/>
         <source>Classic</source>
         <translation>Классический</translation>
     </message>
     <message>
-        <location filename="../qml/harbour-2048.qml" line="12"/>
         <source>Adventure</source>
         <translation>Приключение</translation>
     </message>
     <message>
-        <location filename="../qml/harbour-2048.qml" line="13"/>
         <source>Easy</source>
         <translation>Простая</translation>
     </message>
     <message>
-        <location filename="../qml/harbour-2048.qml" line="13"/>
         <source>Normal</source>
         <translation>Нормальная</translation>
     </message>
     <message>
-        <location filename="../qml/harbour-2048.qml" line="13"/>
         <source>Hard</source>
         <translation>Сложная</translation>
     </message>
     <message>
-        <location filename="../qml/harbour-2048.qml" line="14"/>
         <source>TetraTile</source>
         <translation>Квадратные плитки</translation>
     </message>
     <message>
-        <location filename="../qml/harbour-2048.qml" line="14"/>
         <source>HexaTile</source>
         <translation>Шестиугольные плитки</translation>
     </message>
